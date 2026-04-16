1643a0aa32e1943f48892c7f5971295c86d3a047
hellerdavidprog <134982749+hellerdavidprog@users.noreply.github.com>
2026-04-16 13:38:00 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        url: '/favicon.ico',
+      },
+      {
         url: '/icon-light-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
